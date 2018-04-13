fc5aadde1d2e715b75d70458537116d45a1894ec
Renan Marcel Paulino leite <renan.leite@globo.com>
2018-04-13 02:03:19 -0300

    feat: correção de CI

--- build.sh
+++ build.sh
@@ -3,6 +3,22 @@
 #exit if any command fails
 set -e
 
+artifactsFolder="./artifacts"
+
+if [ -d $artifactsFolder ]; then  
+  rm -R $artifactsFolder
+fi
+
 dotnet restore
 
-dotnet test test/test.csproj
+# Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
+# but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
+
+dotnet test test/test.csproj -c Release -f netcoreapp2.0
+
+dotnet build .\src\AdminProj.csproj -c Release /p:DeployOnBuild=true  --framework netcoreapp2.0
+
+revision=${TRAVIS_JOB_ID:=1}  
+revision=$(printf "%04d" $revision) 
+
+dotnet pack ./src/AdminProj.csproj -c Release /p:DeployOnBuild=true -o ../artifacts --version-suffix=$revision  
